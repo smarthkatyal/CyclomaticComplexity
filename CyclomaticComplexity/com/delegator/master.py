f2fcfef4f176d4251c4c3053dcfef8d709ff9c30
Smarth Katyal <smarth.katyal@gmail.com>
2017-12-15 20:11:48 +0000

    Made master server to get commit sha's for all commits, now send sha to slave

--- CyclomaticComplexity/com/delegator/master.py
+++ CyclomaticComplexity/com/delegator/master.py
@@ -1,5 +1,26 @@
-'''
-Created on 15-Dec-2017
+import json, requests
+from flask import Flask
 
-@author: Smarth Katyal
-'''
+app = Flask(__name__)
+
+class master():
+    def __init__(self):
+        repoShortPath="smarthkatyal/FileSystem/"
+        self.numWorkers = 1
+        self.currNumWorkers = 0  # Number of workers who have connected to the manager
+        self.startTime = 0.0  # Start time for the timer
+        # request repository info using the github API
+        self.shaList = []  # List containing all commit sha values
+        r = requests.get("https://api.github.com/repos/{}commits?page=1&per_page=200".format(repoShortPath)) 
+        commits = json.loads(r.text)
+        for i in commits:
+            self.shaList.append(i['sha'])
+            print("Commit Sha: {}".format(i['sha']))
+        print("\nNumber of commits:{}".format(len(self.shaList)))
+        self.listOfCCs = []
+        
+
+
+if __name__ == "__main__":
+    masterObj = master()  # ini an instance of managerServer()
+app.run(port=8085) # int(sys.argv[1])  , debug=True
