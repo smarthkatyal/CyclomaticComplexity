95b20e70a642906a9542f67d5812181f9caee2b5
Smarth Katyal <smarth.katyal@gmail.com>
2017-12-15 20:46:25 +0000

    Made changes to get repo from master.

--- CycloCalculator/com/analyzer/slave.py
+++ CycloCalculator/com/analyzer/slave.py
@@ -1,13 +1,18 @@
-import os, subprocess, requests
+import json, os, subprocess, requests
 from tkinter.tix import Shell
 import os
 
 def run():
+    masterip="127.0.0.1"
+    masterport="8085"
     command = "rmdir /s /q downloadedCode"
     os.system(command)
     command ="mkdir downloadedCode && chdir downloadedCode"
     os.system(command)
-    command="chdir downloadedCode && git init && git remote add origin {} && git pull".format("https://github.com/smarthkatyal/FileSystem")
+    repo = requests.get("http://{}:{}/repo".format(masterip,masterport), json={'repoState': 0})
+    json_data = json.loads(repo.text)
+    url = json_data['repoUrl']
+    command="chdir downloadedCode && git init && git remote add origin {} && git pull".format(url)
     os.system(command)
     sha = "480fa7bda123368fe7cdf87d46ddb62e452e0a1c"
     command2 = "cd downloadedCode && git reset --hard {}".format(sha)
--- CyclomaticComplexity/com/delegator/master.py
+++ CyclomaticComplexity/com/delegator/master.py
@@ -1,13 +1,42 @@
 import json, requests
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+api = Api(app)
 
+class initializer(Resource):
+    def __init__(self):  
+        global masterObj 
+        self.server = masterObj
+        super(initializer, self).__init__() 
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument('repoState', type=int, location = 'json')  ##Whether slave has downloaded the repository
+        self.reqparser.add_argument('CCN', type=float, location='json') ##Whether slave has calculated the complexity
+
+    ##When slaves connects, this is the first function it will call in master, master will the slaves a repo
+    def get(self):
+        args = self.reqparser.parse_args()
+        if args['repoState'] == 0:  ###slave does not have repo, then send
+            print("Got a new slave, sending repo to pull...")
+            return {'repoUrl': "https://github.com/smarthkatyal/FileSystem"}  # 'https://github.com/fchollet/deep-learning-models'
+        if args['repoState'] == 1:  ##slave has the repo, then just incriment ready slaves counter
+            self.server.gotSlaves += 1
+        print("Slaves obtained: {}".format(self.server.gotSlaves))
+    def post(self):
+        pass
+api.add_resource(initializer, "/repo", endpoint="repo")
+
+
+    
 class master():
     def __init__(self):
+        #Below should be configurable
         repoShortPath="smarthkatyal/FileSystem/"
-        self.numWorkers = 1
-        self.currNumWorkers = 0  # Number of workers who have connected to the manager
+        self.totalSlaves = 1
+        #Configurable part ends
+        
+        self.gotSlaves = 0  # Number of workers who have connected to the manager
         self.startTime = 0.0  # Start time for the timer
         # request repository info using the github API
         self.shaList = []  # List containing all commit sha values
