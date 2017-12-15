03c044837579b89fb5c4a83bba4ecf1ab8651307
Smarth Katyal <smarth.katyal@gmail.com>
2017-12-15 19:44:15 +0000

    Got final CCN value

--- CycloCalculator/com/analyzer/slave.py
+++ CycloCalculator/com/analyzer/slave.py
@@ -1,4 +1,5 @@
 import os, subprocess
+from subprocess  import Popen, PIPE
 command = "rmdir /s /q downloadedCode"
 os.system(command)
 command ="mkdir downloadedCode && chdir downloadedCode"
@@ -8,4 +9,8 @@ os.system(command)
 sha = "480fa7bda123368fe7cdf87d46ddb62e452e0a1c"
 command2 = "cd downloadedCode && git reset --hard {}".format(sha)
 os.system(command2)
-out = subprocess.call(["lizard","downloadedCode"])
+out = subprocess.check_output(["lizard","downloadedCode"])
+out = out.decode("utf-8")
+lastline = out.splitlines()[-1:]
+CCN = lastline[0].split()[2]
+print(CCN)
