c21ea8d49c5dfeb3fd0a58643f3017d915078a5b
Smarth Katyal <smarth.katyal@gmail.com>
2017-12-15 19:52:52 +0000

    Created Master project, renamed slave project

--- CycloCalculator/com/analyzer/slave.py
+++ CycloCalculator/com/analyzer/slave.py
@@ -1,16 +1,22 @@
-import os, subprocess
-from subprocess  import Popen, PIPE
-command = "rmdir /s /q downloadedCode"
-os.system(command)
-command ="mkdir downloadedCode && chdir downloadedCode"
-os.system(command)
-command="chdir downloadedCode && git init && git remote add origin {} && git pull".format("https://github.com/smarthkatyal/FileSystem")
-os.system(command)
-sha = "480fa7bda123368fe7cdf87d46ddb62e452e0a1c"
-command2 = "cd downloadedCode && git reset --hard {}".format(sha)
-os.system(command2)
-out = subprocess.check_output(["lizard","downloadedCode"])
-out = out.decode("utf-8")
-lastline = out.splitlines()[-1:]
-CCN = lastline[0].split()[2]
-print(CCN)
+import os, subprocess, requests
+from tkinter.tix import Shell
+import os
+
+def run():
+    command = "rmdir /s /q downloadedCode"
+    os.system(command)
+    command ="mkdir downloadedCode && chdir downloadedCode"
+    os.system(command)
+    command="chdir downloadedCode && git init && git remote add origin {} && git pull".format("https://github.com/smarthkatyal/FileSystem")
+    os.system(command)
+    sha = "480fa7bda123368fe7cdf87d46ddb62e452e0a1c"
+    command2 = "cd downloadedCode && git reset --hard {}".format(sha)
+    os.system(command2)
+    out = subprocess.check_output(["lizard","downloadedCode"])
+    out = out.decode("utf-8")
+    lastline = out.splitlines()[-1:]
+    CCN = lastline[0].split()[2]
+    print(CCN)
+
+if __name__ == "__main__":
+    run()
